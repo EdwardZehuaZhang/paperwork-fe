39f70eb003a7dececd5a8ec8c40b42ce2fd08f06
Zhang Zehua <edward.zehua.zhang@gmail.com>
2025-12-20 04:58:16 +0800

    feat: w

--- paperwork-fe/apps/frontend/src/app/layouts/app-layout.tsx
+++ paperwork-fe/apps/frontend/src/app/layouts/app-layout.tsx
@@ -34,29 +34,35 @@ function getBreadcrumbTitle(pathname: string): string {
 
 export function AppLayout() {
   const location = useLocation();
+  const hideChrome = /^\/workflows\/[^/]+\/?$/.test(location.pathname);
   const breadcrumbTitle = getBreadcrumbTitle(location.pathname);
+  const contentClassName = hideChrome
+    ? 'flex flex-1 flex-col'
+    : 'flex flex-1 flex-col gap-4 p-4 pt-0';
 
   return (
     <SidebarProvider>
-      <AppSidebar />
+      {!hideChrome && <AppSidebar />}
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbPage>{breadcrumbTitle}</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        {!hideChrome && (
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{breadcrumbTitle}</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+        )}
+        <div className={contentClassName}>
           <Outlet />
         </div>
       </SidebarInset>
